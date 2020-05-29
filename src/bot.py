a28b7c7b0c53dd70ef510ab596069ff94017dda2
Simon <trilogicworlds@gmail.com>
2020-05-29 12:57:17 +0200

    add euler problem generator

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,11 @@
 import json
 import random
-import winsound
-from time import sleep
-
+import discord  # required for pretty imbeds
 import requests
+
 from discord.ext import commands
+from bs4 import BeautifulSoup
+from time import sleep
 
 with open("DISCORD_TOKEN.txt", "r") as code:
     TOKEN = code.readlines()[0]
@@ -12,33 +13,24 @@ with open("DISCORD_TOKEN.txt", "r") as code:
 bot = commands.Bot(command_prefix=',')
 
 
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
-
-
-@bot.command(name='timer')
-async def timer(ctx, minutes=25):
+@bot.command(name='timer', brief='Pomodoro-esque timer for productivity!', description='Run a timer for x minutes and be alerted when your time is up.')
+async def timer(ctx, minutes=1):
     await ctx.send(f'Timer for {minutes} minutes has started!')
     time = minutes * 60
     for x in range(time):
         while time > 0:
             time -= 1
             sleep(1)
-            print(time)
         if time == 0:
-            await ctx.send('Times up!!!')
-            winsound.Beep(400, 200)  # A short beep to show that the timer ended
-            break
+            await ctx.send('\n\nTimes up!!!\nContinue?')
+            # add reactions thumbsup and thumbsdown
+            # check if user reacted to emojis
 
 
-@bot.command(name='github')
+@bot.command(name='github', brief='See top GitHub repos', description='Return the top daily GitHub repos!')
 async def fetch(ctx):
-    page = requests.get('https://github-trending-api.now.sh/repositories?q=sort=stars&order=desc&since=daily')
+    page = requests.get(
+        'https://github-trending-api.now.sh/repositories?q=sort=stars&order=desc&since=daily')
     jsonpage = json.loads(page.content)
     pretty_list = []
     lst = ([(repo["name"], repo["author"]) for repo in jsonpage])
@@ -47,15 +39,38 @@ async def fetch(ctx):
         ele = ' '.join(ele[i] for i in range(len(ele)))
         pretty_list.append(ele)
     for char in str(pretty_list):
-        if char not in good_chars: pretty_list = str(pretty_list).replace(char, ' ')
+        if char not in good_chars:
+            pretty_list = str(pretty_list).replace(char, ' ')
     await ctx.send(pretty_list)
 
+# access current number of Project Euler problems
+link = 'https://projecteuler.net/archives'
+page = requests.get(link).content
+soup = BeautifulSoup(page, 'html.parser')
+lst = list(soup.select('li'))
+lst[0] = str(lst[0])
+lst[0] = lst[0].split(" ")
+num_problems = int(lst[0][1])
 
-@bot.command(name='eval')
-async def run(ctx, content='"Content not set"'):
-    output = eval(content)
-    await ctx.send(f'Output: {output}')
+
+@bot.command(name='euler', brief='Get specific or random Project Euler problem', description='Sends the user a specific or random Project Euler challenge')
+async def find_problem(ctx, number=random.randint(0, num_problems+1)):
+    link = f'https://projecteuler.net/problem={number}'
+    page = requests.get(link).content
+    soup = BeautifulSoup(page, 'html.parser')
+    problem_number = soup.title.text
+    problem_content = soup.select_one('.problem_content').text
+
+    if len(problem_content) < 1024:  # fits in embed
+        embed = discord.Embed(
+            title=problem_number, description='The Project Euler problem that you requested. Have fun programming!', color=0x00ff00)
+
+        embed.add_field(name="Problem Content",
+                        value=f'{problem_content}', inline=False)
+        await ctx.send(embed=embed)
+
+    else:
+        await ctx.send(f'{problem_number}\n\n{problem_content}')
 
 
-# client.run(TOKEN)  # way1
 bot.run(TOKEN)  # way2
