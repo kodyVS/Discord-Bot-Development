0b25ac8204dab25cced91a956d14e3ee67d7c025
ashdrex <a.shu.li@live.com>
2020-05-29 00:11:01 -0400

    ......

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,11 @@
-from discord.ext import commands
-import random
-
-from time import sleep
-import requests
-from bs4 import BeautifulSoup
 import json
+import random
 import winsound
+from time import sleep
+
+import requests
+from discord.ext import commands
+from discord.utils import get
 
 with open("DISCORD_TOKEN.txt", "r") as code:
     TOKEN = code.readlines()[0]
@@ -20,35 +20,67 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
+
+@bot.command()
+async def join(ctx):
+    try:
+        channel = ctx.message.author.voice.channel
+        voice = get(bot.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            await channel.connect()
+    except AttributeError:
+        await ctx.send(f'You are not in a voice channel.')
+
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    if ctx.message.author.voice:
+        server = ctx.message.guild.voice_client
+        await server.disconnect()
+
+
 @bot.command(name='timer')
-async def timer(ctx, minutes=25):
-    await ctx.send(f'Timer for {minutes} minutes has started!')
-    time = minutes*60
-    for x in range(time):
-        while time > 0:
-            time-=1
-            sleep(1)
-            print(time)
-        if time == 0:
-            await ctx.send('Times up!!!')
-            winsound.Beep(400, 200) #A short beep to show that the timer ended
-            break
+async def timer(ctx, minutes):
+    try:
+        int(minutes)
+        await ctx.send(f'Timer for {minutes} minutes has started!')
+        time = int(minutes) * 60
+        for x in range(time):
+            while time > 0:
+                time -= 1
+                sleep(1)
+                print(time)
+            if time == 0:
+                await ctx.send('Times up!!!')
+                winsound.Beep(400, 200)  # A short beep to show that the timer ended
+                break
+    except ValueError:
+        await ctx.send(f'Invalid time set.')
+
+
 @bot.command(name='github')
 async def fetch(ctx):
     page = requests.get('https://github-trending-api.now.sh/repositories?q=sort=stars&order=desc&since=daily')
-    jsonpage =  json.loads(page.content)
+    jsonpage = json.loads(page.content)
     pretty_list = []
     lst = ([(repo["name"], repo["author"]) for repo in jsonpage])
     good_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '
     for ele in lst:
-      ele = ' '.join(ele[i] for i in range(len(ele)))
-      pretty_list.append(ele)
+        ele = ' '.join(ele[i] for i in range(len(ele)))
+        pretty_list.append(ele)
     for char in str(pretty_list):
-        if char not in  good_chars: pretty_list = str(pretty_list).replace(char, ' ')
+        if char not in good_chars: pretty_list = str(pretty_list).replace(char, ' ')
     await ctx.send(pretty_list)
+
+
 @bot.command(name='eval')
 async def run(ctx, content='"Content not set"'):
     output = eval(content)
     await ctx.send(f'Output: {output}')
+
+
 # client.run(TOKEN)  # way1
 bot.run(TOKEN)  # way2
