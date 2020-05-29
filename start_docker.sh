613bfc5c8e1a2f8d2bd8ca443a046acb48672d9f
Kolos Niedermayer <kolos.niedermayer@gmail.com>
2020-05-29 20:00:03 +0200

    modified shell script to build and run if the container is not created yet else it just runs the container

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-docker run -itd --name pd_bot -v $(pwd):/app pg_bot
+CONTAINER_NAME="pg_bot"
+grep_container=$(docker images | grep -i "${CONTAINER_NAME}")
+len_str=${#grep_container}
+
+check_container(){
+	if [ $len_str -ne 0 ]
+		then
+			return 0
+		else
+			return 1
+	fi
+}
+
+build_and_run(){
+	docker build -t pg_bot . &&
+	docker run -itd --name pg_bot -v $(pwd):/app pg_bot
+}
+
+
+if check_container
+	then docker start pg_bot
+	else build_and_run 
+fi
+
