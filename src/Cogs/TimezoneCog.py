ccf74833f8fb158a42a9d30ebf21f45885f376d3
Simon <trilogicworlds@gmail.com>
2020-06-05 18:25:57 +0200

    Co-authored-by: Ash <a.shu.li@live.com>

--- src/Cogs/TimezoneCog.py
+++ src/Cogs/TimezoneCog.py
@@ -38,13 +38,13 @@ class TimezoneCog(commands.Cog):
         now_time = dt.now(timezone(zone))
         embed = discord.Embed(
             title=f"{zone} Time",
-            description=f"The current time for {zone} is **{now_time.strftime(fmt)}**",
+            description=f"The current time for **{zone}** is **{now_time.strftime(fmt)}**",
             color=0x00FF00,
         )
         await ctx.send(embed=embed)
 
     def possible_timezones(self, tz_offset, common_only=True):
-    # pick one of the timezone collections
+        # pick one of the timezone collections
         timezones = pytz.common_timezones if common_only else pytz.all_timezones
 
         # convert the float hours offset to a timedelta
@@ -59,7 +59,7 @@ class TimezoneCog(commands.Cog):
         results = []
         for tz_name in timezones:
             tz = timezone(tz_name)
-            non_dst_offset = getattr(tz, '_transition_info', [[null_delta]])[-1]
+            non_dst_offset = getattr(tz, "_transition_info", [[null_delta]])[-1]
             if desired_delta == non_dst_offset[0]:
                 results.append(tz_name)
 
@@ -68,23 +68,21 @@ class TimezoneCog(commands.Cog):
     @commands.command(
         name="addtime",
         brief="Add your timezone to your guilds timezone",
-        description="Makes your timezone publicly accessible",
-        aliases=["mytime", "myhour", "mytimezone", "addmytimezone"],
+        description="Makes your timezone publicly accessible. Input either your timezone as a region or the current hour",
+        aliases=["mytime", "myhour", "mytimezone", "addmytimezone", "settime"],
     )
-    async def my_time(self, ctx, zone: str = 'lazy'):
+    async def my_time(self, ctx, zone):
 
-        if zone == 'lazy':
-            hour_diff = time.localtime().tm_gmtoff/3600 # time offset in hours
-            zone = self.possible_timezones(hour_diff)[0]
-
-        myzonedict = {
-            f"{ctx.author.name}": f"{zone}"
-        }  # FIXME: support for people who don't know their timezones
+        myzonedict = {f"{ctx.author.name}": [f"{zone}", f"{ctx.message.guild.id}"]}
 
         for x in self.col.find():
-            for key in x.keys():
-                if key == ctx.author.name:
-                    self.col.delete_one(x)
+            if list(x.keys())[1] == str(
+                ctx.author.name
+            ):  # check if USERNAME is there, not guild...
+            
+                for key in x.keys():
+                    if key == ctx.author.name:
+                        self.col.delete_one(x)
 
         self.col.insert_one(myzonedict)
 
@@ -101,9 +99,11 @@ class TimezoneCog(commands.Cog):
 
         fmt = "%H:%M"
         for x in self.col.find({}, {target}):
-            result_obj = x
 
-        result_timezone = result_obj[target]
+            if x[target][1] == str(ctx.message.guild.id):
+                result_obj = x[target]
+
+        result_timezone = result_obj[0]
         tz = timezone(result_timezone)
         tz_now = dt.now(tz)
 
@@ -119,19 +119,25 @@ class TimezoneCog(commands.Cog):
         name="alltimes",
         brief="See all times of everyone registered",
         description="Shows the time of every registered user in this server.",
-        aliases = ['everytime', 'alltime', 'guiltimes', 'alltimezones', 'alltimezone']
+        aliases=["everytime", "alltime", "guiltimes", "alltimezones", "alltimezone", "timelist"],
     )
     async def all_times(self, ctx):
         time_list = []
-        fmt = "%H:%M"
+        fmt = "%H:%M %Z"
 
-        for x in self.col.find():
-            time_list.append(list(x.items())[1])  # list with usernames and timezones
+        for x in self.col.find():  # FIXME: damn bug that just won't go away
+            prettified = list(x.items())
+
+            if prettified[1][1][1] == str(ctx.message.guild.id):
+                time_list.append(
+                    list(x.items())[1]
+                )  # list with usernames and timezones
 
         # prettify output if it can fit in the embed
         if len(str(time_list)) < 1024:
             for i in range(len(time_list)):
-                tz = time_list[i][1]
+
+                tz = time_list[i][1][0]
                 tz = timezone(tz)
                 tz_now = dt.now(tz)
                 time_list[i] = str(
@@ -152,6 +158,10 @@ class TimezoneCog(commands.Cog):
 
             await ctx.send(time_list)
 
-    @commands.command(name = 'convertimezone', brief = 'Converts between timezones', aliases = ['ctz', 'convertime', 'convertz', 'comparetimezones'])
+    @commands.command(
+        name="convertimezone",
+        brief="Converts between timezones",
+        aliases=["ctz", "convertime", "convertz", "comparetimezones"],
+    )
     async def compare_timezones(self, ctx, *args):
         pass
