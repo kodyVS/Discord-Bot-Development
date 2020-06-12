a45cec5ac9e0da2d86a16c7c37e75f044635d9a1
Christian <christian.vandijk@student.hu.nl>
2020-06-12 11:55:30 +0200

    begun refactoring into service

---
+++ src/Cogs/FileStorageService/FileStorageCog.py
@@ -0,0 +1,52 @@
+import io
+import os
+
+import discord
+import gridfs
+import requests
+from discord.ext import commands
+from pymongo import MongoClient
+from Cogs.FileStorageService.FileStorageService import FileStorageService
+
+
+class FileStorageCog(commands.Cog, name='FileStorageCog'):
+    def __init__(self, bot):
+        self.bot = bot
+        self.file_storage = FileStorageService()
+
+    @staticmethod
+    async def return_error(ctx, file_name=None):
+        embed = discord.Embed(title=f"Couldn't find file '{file_name}'" if file_name is not None else
+        "Couldn't find any files", color=0xff0000)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='store', brief='Store a file',
+                      description='Store a file to the current Guild',
+                      aliases=['fsput', 'putfile', 'save', 'savefile'])
+    async def insert_file(self, ctx, filename=None):
+        await self.file_storage.insert_file(ctx, filename)
+
+    @commands.command(name='get', brief='Retrieve a file',
+                      description='Retrieves a file of the current Guild by filename',
+                      aliases=['fsget', 'getfile', 'retrieve'])
+    async def get_file(self, ctx, file_name):
+        await self.file_storage.get_file(ctx, file_name)
+        
+
+    @commands.command(name='list', brief='list files',
+                      description='lists files of the current guild',
+                      aliases=['fslist', 'listfile'])
+    async def list_files(self, ctx):
+        await self.file_storage.list_files(ctx)
+
+    @commands.command(name='delete', brief='delete file',
+                      description="delete file from guild by filename\n using filename 'all' will delete all files",
+                      aliases=['fsdelete', 'deletefile'])
+    async def delete_files(self, ctx, file_name):
+        if file_name == "all":
+            await self.file_storage.delete_all(ctx)
+        else:
+            try:
+                await self.file_storage.delete_one(ctx, file_name)
+            except:
+                await self.return_error(ctx, file_name)
--- src/Cogs/FileStorageService/FileStorageService.py
+++ src/Cogs/FileStorageService/FileStorageService.py
@@ -8,9 +8,8 @@ from discord.ext import commands
 from pymongo import MongoClient
 
 
-class FileStorageCog(commands.Cog, name='FileStorageCog'):
-    def __init__(self, bot):
-        self.bot = bot
+class FileStorageService(object):
+    def __init__(self):
         try:
             mongo_uri = os.environ.get('MONGO_URI')
         except KeyError:
@@ -26,13 +25,10 @@ class FileStorageCog(commands.Cog, name='FileStorageCog'):
         "Couldn't find any files", color=0xff0000)
         await ctx.send(embed=embed)
 
-    @commands.command(name='store', brief='Store a file',
-                      description='Store a file to the current Guild',
-                      aliases=['fsput', 'putfile', 'save', 'savefile'])
-    async def insert_file(self, ctx, filename=None):
+    async def insert_file(self, ctx, file_name):
         upload_url = ctx.message.attachments[0].url
         file = requests.get(upload_url)
-        file_name = filename if filename is not None else ctx.message.attachments[0].filename
+        file_name = file_name if file_name is not None else ctx.message.attachments[0].filename
 
         # check double file names
         for grid_out in self.fs.find({"filename": file_name}, no_cursor_timeout=True):
@@ -51,9 +47,6 @@ class FileStorageCog(commands.Cog, name='FileStorageCog'):
             color=0x00ff00)
         await ctx.send(embed=embed)
 
-    @commands.command(name='get', brief='Retrieve a file',
-                      description='Retrieves a file of the current Guild by filename',
-                      aliases=['fsget', 'getfile', 'retrieve'])
     async def get_file(self, ctx, file_name):
         file = self.fs.find_one({"filename": file_name})
         try:
@@ -66,20 +59,17 @@ class FileStorageCog(commands.Cog, name='FileStorageCog'):
                     is_image = False
 
                 if is_image:
-                    embed = discord.Embed(title="File Delivery!", color=0x00ff00).set_image(url=file.upload_url)
+                    embed = discord.Embed(title=file.filename, description="File Delivery!", color=0x00ff00).set_image(url=file.upload_url)
                     await ctx.send(embed=embed)
                 # idea to return embedded MarkDown if it's a md file
                 else:
-                    await ctx.send(embed=discord.Embed(title="File Delivery!", color=0x00ff00))
+                    await ctx.send(embed=discord.Embed(title=file.filename, description="File Delivery!", color=0x00ff00))
                     await ctx.send(file=raw_file)
             else:
                 await self.return_error(ctx, file_name)
         except:
             await self.return_error(ctx, file_name)
 
-    @commands.command(name='list', brief='list files',
-                      description='lists files of the current guild',
-                      aliases=['fslist', 'listfile'])
     async def list_files(self, ctx):
         message = None
         for grid_out in self.fs.find({"guild_id": ctx.guild.id}, no_cursor_timeout=True):
@@ -89,16 +79,18 @@ class FileStorageCog(commands.Cog, name='FileStorageCog'):
         if message is None:
             await self.return_error(ctx)
 
-    @commands.command(name='delete', brief='delete file',
-                      description='delete file from guild by filename',
-                      aliases=['fsdelete', 'deletefile'])
-    async def delete_files(self, ctx, file_name):
+    async def delete_all(self, ctx):
+        for grid_out in self.fs.find({"guild_id": ctx.guild.id}, no_cursor_timeout=True):
+            self.fs.delete(grid_out._id)
+            embed = discord.Embed(title=f"Deleted: {grid_out.filename}", color=0xff0000).set_thumbnail(
+                url=grid_out.upload_url)
+            self.fs.delete(grid_out._id)
+            await ctx.send(embed=embed)
+
+    async def delete_one(self, ctx, file_name):
         file = self.fs.find_one({"filename": file_name})
-        try:
-            if file.guild_id == ctx.guild.id:
-                embed = discord.Embed(title=f"Deleted: {file.filename}", color=0xff0000).set_thumbnail(
-                    url=file.upload_url)
-                self.fs.delete(file._id)
-                await ctx.send(embed=embed)
-        except:
-            await self.return_error(ctx, file_name)
+        if file.guild_id == ctx.guild.id:
+            embed = discord.Embed(title=f"Deleted: {file.filename}", color=0xff0000).set_thumbnail(
+                url=file.upload_url)
+            self.fs.delete(file._id)
+            await ctx.send(embed=embed)
